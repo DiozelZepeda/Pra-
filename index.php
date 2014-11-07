9c6cff17ed5fd61765331aad0089951faa643d11
DiozelZepeda <nereide2007@gmail.com>
2014-11-06 22:40:17 -0300

    Coming 2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
  *
- * You can load different configurations depending on your
+ * Yogu can load different configurations depending on your
  * current environment. Setting the environment also influences
  * things like logging and error reporting.
  *
